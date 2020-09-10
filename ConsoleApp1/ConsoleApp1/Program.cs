89406aa9353a0fe6a7385e5f849c658d6e16e4ed
artobnje22 <70949554+artobnje22@users.noreply.github.com>
2020-09-10 15:20:15 +0530

    jenkins

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public void getNumbers()
+        {
+            int a = 0;
+            a = int.MaxValue;
+            Console.WriteLine(a);
+            checked
+            {
+                a++;
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            (new Program()).getNumbers();
+        }
+    }
+}
